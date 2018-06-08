95250e72bfe4a93dfa56098a7453597001c5e3ad
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-09 03:02:15 +0900

    :new: プロジェクト追加

---
+++ Assets/RL/Scripts/ObjectPools/PoolableEffect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using HK.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Joule.ObjectPools
+{
+    /// <summary>
+    /// プール可能なエフェクト
+    /// </summary>
+    public sealed class PoolableEffect : MonoBehaviour
+    {
+        public static readonly ObjectPoolBundle<PoolableEffect> Bundle = new ObjectPoolBundle<PoolableEffect>();
+
+        private ObjectPool<PoolableEffect> pool;
+
+        private Transform cachedTransform;
+
+        void Awake()
+        {
+            this.cachedTransform = this.transform;
+        }
+
+        public PoolableEffect Rent(Vector3 position, Quaternion rotation)
+        {
+            var pool = Bundle.Get(this);
+            var instance = pool.Rent();
+            instance.pool = pool;
+            instance.cachedTransform.position = position;
+            instance.cachedTransform.rotation = rotation;
+
+            return instance;
+        }
+
+        public IDisposable ReturnToPoolOnTimer(float seconds)
+        {
+            return Observable.Timer(TimeSpan.FromSeconds(seconds))
+                .SubscribeWithState(this, (_, _this) => { _this.pool.Return(_this); })
+                .AddTo(this);
+        }
+    }
+}
